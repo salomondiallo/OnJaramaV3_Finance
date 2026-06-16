41fa74f9ace1bad3488f7fb73e4d0ff45c546c22
Salomon Diallo <tsdiallo3@gmail.com>
2026-06-15 20:26:16 -0400

    Remove all hardcoded personal data

--- src/data/defaultFinanceData.js
+++ src/data/defaultFinanceData.js
@@ -1,24 +1,9 @@
 export const defaultFinanceData = {
   overview: {
-    monthlyIncome: 3180,
-    monthlyExpenses: 1380,
-    monthlySavings: 650,
+    monthlyIncome: 0,
+    monthlyExpenses: 0,
+    monthlySavings: 0,
   },
 
-  debts: [
-    {
-      name: "Fairstone",
-      type: "Prêt personnel",
-      balance: 5257.93,
-      interestRate: 34.99,
-      minimumPayment: 227.75,
-    },
-    {
-      name: "Desjardins Modulo",
-      type: "Marge de crédit",
-      balance: 13100,
-      interestRate: 12,
-      minimumPayment: 671,
-    },
-  ],
+  debts: [],
 };
--- src/data/salomonFinance.js
+++ src/data/salomonFinance.js
@@ -1,46 +1,16 @@
 export const financeData = {
-  user: "Salomon",
+  user: "Utilisateur Démo",
 
   overview: {
-    monthlyIncome: 3180,
-    monthlyExpenses: 1280,
-    monthlySavings: 650,
-    totalDebt: 19671,
-    progressScore: 75,
-    nextAction: "Paiement Fairstone",
+    monthlyIncome: 0,
+    monthlyExpenses: 0,
+    monthlySavings: 0,
+    totalDebt: 0,
+    progressScore: 0,
+    nextAction: "Commencer votre parcours",
   },
 
-  debts: [
-    {
-      name: "Fairstone",
-      balance: 5257.93,
-      interestRate: 34.99,
-      priority: "haute",
-    },
-    {
-      name: "Modulo",
-      balance: 13400,
-      interestRate: 12,
-      priority: "moyenne",
-    },
-  ],
+  debts: [],
 
-  goals: [
-    {
-      name: "Sortir des dettes",
-      progress: 75,
-    },
-    {
-      name: "Maison Guinée",
-      progress: 25,
-    },
-    {
-      name: "Retour en Guinée",
-      progress: 10,
-    },
-    {
-      name: "Retraite",
-      progress: 5,
-    },
-  ],
+  goals: [],
 };
